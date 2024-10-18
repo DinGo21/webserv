a6ee31e276520edb41863099eae51f8cd8ff9a37
Diego <diego.antoniosantamaria@gmail.com>
2024-10-18 11:09:52 +0200

    config

--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: disantam <disantam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: diego <diego@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 12:56:34 by disantam          #+#    #+#             */
-/*   Updated: 2024/10/16 16:34:35 by disantam         ###   ########.fr       */
+/*   Updated: 2024/10/18 11:05:50 by diego            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
 #include "Server.hpp"
 
-int	get_config(std::ifstream &config)
+void	parse_args(Server &server, std::vector<std::string> &args)
+{
+	
+}
+
+std::vector<std::string>	get_config(std::ifstream &config)
 {
 	size_t						i = 0;
 	size_t						j = 0;
 	std::string					tmp;
 	std::string					raw;
-	std::vector<std::string>	args;	
+	std::vector<std::string>	args;
 
 	while (std::getline(config, tmp))
 	{
 		raw += tmp;
 		raw.push_back('\n');
 	}
-	std::cout << raw << std::endl;
-	std::cout << raw.size() << std::endl;
 	while (i < raw.size())
 	{
-		while (strchr(" \t\n", raw[i]) != NULL && raw[i] != '\0')
+		while (strchr(" \t\n", raw[i]) && raw[i] != '\0')
 			i++;
 		if (raw[i] == '\0')
 			break ;
 		j = raw.find_first_of(" {};\t\n", i);
-		std::cout << raw[j] << ' ' << j << std::endl;
-		args.push_back(raw.substr(i, j - i));
+		if (i - j)
+			args.push_back(raw.substr(i, j - i));
 		i = j;
-		if (raw[i] == '{' || raw[i] == '}')
+		if (strchr(";{}", raw[i]))
 		{
 			args.push_back(raw.substr(i, 1));
 			i++;
@@ -50,13 +53,14 @@ int	get_config(std::ifstream &config)
 	{
 		std::cout << *it << std::endl;
 	}
-	return 0;
+	return args;
 }
 
 int	main(int argc, char *argv[])
 {
-	// Server			server;
-	std::ifstream	config;
+	Server						server;
+	std::ifstream				config;
+	std::vector<std::string>	args;
 
 	if (argc != 2)
 	{
@@ -69,9 +73,8 @@ int	main(int argc, char *argv[])
 		std::cerr << "Unable to open file: " << argv[1] << std::endl;
 		return 1;
 	}
-	if (get_config(config))
-	{
-		return 1;
-	}
+	args = get_config(config);
+	parse_args(server, args);
+	config.close();
 	return 0;
 }
